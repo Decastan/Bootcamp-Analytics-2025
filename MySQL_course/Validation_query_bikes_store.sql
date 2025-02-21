f3e456f3f583219e422f85a8dda3552cf5dd9186
Daniel Castaño <125098694+Decastan@users.noreply.github.com>
2025-02-20 20:04:23 -0500

    ajustes

--- MySQL_course/Validation_query_bikes_store.sql
+++ MySQL_course/Validation_query_bikes_store.sql
@@ -6,7 +6,12 @@ SELECT
 	c.last_name, 
 	c.city, 
 	c.state,
+	c.total_money_spent,
+	c.total_money_spent + 100 * 10
+FROM bike_store.customers c
+LEFT JOIN bike_store.orders o
     o.*
 FROM bike_store.customers c
 LEFT JOIN orders o
 ON c.customer_id = o.customer_id
+
